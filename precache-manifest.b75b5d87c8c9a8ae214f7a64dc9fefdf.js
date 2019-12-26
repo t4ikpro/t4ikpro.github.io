6285e06a0882595be2b0a7c9e42d4cfe9492229e
M Taris Apryanto 58 <tarissbmptn@gmail.com>
2019-12-26 19:49:38 +0700

    Updates

---
+++ precache-manifest.b75b5d87c8c9a8ae214f7a64dc9fefdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a30c4f4c93ac1326f0f7f71fcc75b42",
+    "url": "/tarisapryanto.github.io/index.html"
+  },
+  {
+    "revision": "c9dc6869bef34a90f98e",
+    "url": "/tarisapryanto.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "174441cf5eab9eaa3cd4",
+    "url": "/tarisapryanto.github.io/static/js/2.b9616606.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tarisapryanto.github.io/static/js/2.b9616606.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9dc6869bef34a90f98e",
+    "url": "/tarisapryanto.github.io/static/js/main.614897d7.chunk.js"
+  },
+  {
+    "revision": "de1bca1a43c3641a7f08",
+    "url": "/tarisapryanto.github.io/static/js/runtime-main.d4081c74.js"
+  }
+]);
