ef4209c1b714112fe40e24a5ada20cc051676338
t4ikpro <59249752+t4ikpro@users.noreply.github.com>
2020-10-28 01:02:55 +0700

    Update sidebar.jsx

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -28,8 +28,7 @@ export default class Sidebar extends Component {
                 <li><a href="https://www.facebook.com/tarisapryanto" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
                 <li><a href="https://twitter.com/@tarisapryanto" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
                 <li><a href="https://www.instagram.com/tarisapryanto/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
-                {/*<li><a href="https://www.linkedin.com/in/ID-DISINI/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>*/}
-                <li><a href="https://github.com/t4ikpro" target="_blank" rel="noopener noreferrer"><i className="icon-github"/></a></li>
+                <li><a href="https://www.github.com/t4ikpro" target="_blank" rel="noopener noreferrer"><i className="icon-github" /></a></li>
                 {/*<li><a href="https://medium.com/ID-DISINI" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>*/}
               </ul>
             </nav>
